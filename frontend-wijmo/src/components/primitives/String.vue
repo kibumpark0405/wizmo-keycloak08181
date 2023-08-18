29e64b2e560e54d6bdf929a103b674c6beafa484
kibumpark0405 <130956306+kibumpark0405@users.noreply.github.com>
2023-08-18 14:22:14 +0900

    commit

---
+++ frontend-wijmo/src/components/primitives/String.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div v-if="editMode">
+            <div class="label-title">{{label}}</div>
+            <v-text-field 
+                v-bind="$attrs"
+                v-model="value"
+                @change="change"
+                label="입력하세요."
+                outlined
+                single-line
+            />
+        </div>
+        <div v-else>
+            {{label}} : {{value}}
+        </div>
+    </div>
+</template>
+<script>  
+    export default {
+        name: 'String',
+        components:{
+        },
+        props: {
+            value:{
+                type: String,
+                default: null /// TODO '' is not null !
+            },
+            editMode: Boolean,
+            label: String,
+        },
+        methods:{
+            change(){
+                if(this.value==='') this.value = null  //TODO '' is not null
+                this.$emit("input", this.value);
+            }
+        }
+    }
+</script>
+<style>
+.v-text-field--outlined > .v-input__control > .v-input__slot{
+    min-height: 34px !important;
+}
+</style>
